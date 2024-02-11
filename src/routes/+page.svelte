f08b1e6698b16120c81b4f2a18ba53b6f8580f58
Denver Fernandes <sub367@gmail.com>
2024-02-11 22:56:37 +0000

    Improve layout and add title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,32 @@
 <script lang="ts">
-    import { enhance } from '$app/forms';
-    
-    export let form;
+    import { enhance } from "$app/forms";
+
+    //TODO: Proper type here
+    export let form: any;
+
+    async function handleCopy() {
+        const encryptedMessageValue = document.getElementById(
+            "encrypted-message",
+        )?.value as string;
+        try {
+            if (navigator.canShare()) {
+                await navigator.share({ text: encryptedMessageValue });
+            } else {
+                await navigator.clipboard.writeText(encryptedMessageValue);
+            }
+        } catch (error) {
+            console.error("Unable to copy. Error was: " + error);
+        }
+    }
 </script>
+<svelte:head>
+    <title>Encrypt - Easy PGP</title> 
+</svelte:head>
 
 {#if form?.errorMessage}
     <div class="rounded-md bg-yellow-50 p-4">
         <div class="flex">
-            <div class="flex-shrink-0">
+            <div class="ml-3 flex flex-row">
                 <svg
                     class="h-5 w-5 text-red-400"
                     viewBox="0 0 20 20"
@@ -20,8 +39,6 @@
                         clip-rule="evenodd"
                     />
                 </svg>
-            </div>
-            <div class="ml-3">
                 <h3 class="text-sm font-medium text-yellow-800">
                     Message encryption error
                 </h3>
@@ -36,41 +53,42 @@
 {/if}
 
 {#if form?.success}
-<div class="rounded-md bg-green-50 p-4">
-    <div class="flex place-content-center">
-      <div class="flex-shrink-0">
-        <svg class="h-5 w-5 text-green-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-          <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
-        </svg>
-      </div>
-      <div class="ml-3">
-        <h3 class="text-sm font-medium text-green-800">Encryption Success</h3>
-        <div class="mt-2 text-sm text-green-700">
-            <textarea
-            name="encrypted-message"
-            id="encrypted-message"
-            readonly
-            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            >
-            {form.encryptedMessage}
-            </textarea>
+    <div class="rounded-md bg-green-50 p-4 flex place-content-center">
+        <div class="ml-3">
+            <div class="mt-2">
+                <h2 class="text-base font-semibold leading-7 text-gray-900">
+                    Encryption success
+                </h2>
+                <p class="mt-1 text-sm leading-6 text-gray-600">
+                    Your message has been sucessfully encrypted for the
+                    recipient.
+                </p>
+                <textarea
+                    name="encrypted-message"
+                    id="encrypted-message"
+                    readonly
+                    class="block w-full min-h-56 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                >
+                    {form.encryptedMessage}
+                </textarea>
+            </div>
+            <div class="mt-4">
+                <div class="-mx-2 -my-1.5 flex">
+                    <button
+                        on:click={handleCopy}
+                        type="button"
+                        class="rounded-md bg-green-50 px-2 py-1.5 text-sm font-medium text-green-800 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50"
+                        >Copy to clipboard</button
+                    >
+                </div>
+            </div>
         </div>
-        <div class="mt-4">
-          <div class="-mx-2 -my-1.5 flex">
-            <button type="button" class="rounded-md bg-green-50 px-2 py-1.5 text-sm font-medium text-green-800 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50">Copy to clipboard</button>
-          </div>
-        </div>
-      </div>
     </div>
-  </div>
-  
 {:else}
     <form method="POST" use:enhance>
         <div class="flex flex-col space-y-12">
             <div class="border-b border-gray-900/10 pb-12 m-8">
-                <div
-                    class="mt-10 flex place-content-center"
-                >
+                <div class="mt-10 flex place-content-center">
                     <div class="">
                         <h2
                             class="text-base font-semibold leading-7 text-gray-900"
@@ -78,12 +96,11 @@
                             Encrypt
                         </h2>
                         <p class="mt-1 text-sm leading-6 text-gray-600">
-                            Use this tool to encrypt a message with the recipient's public key.
+                            Use this tool to encrypt a message with the
+                            recipient's public key.
                         </p>
 
-                        <div
-                            class="mt-5 flex flex-col"
-                        >
+                        <div class="mt-5 flex flex-col">
                             <div class="sm:col-span-3">
                                 <label
                                     for="message"
@@ -96,6 +113,7 @@
                                         id="message"
                                         class="block w-full min-h-52 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                         required
+                                        value={form?.message ?? ""}
                                     />
                                 </div>
                             </div>
@@ -122,7 +140,7 @@
                     <button
                         type="submit"
                         class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        >Save</button
+                        >Encrypt</button
                     >
                 </div>
             </div>
