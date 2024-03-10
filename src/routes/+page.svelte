b4dfc230382f31e22515421d7acc709536d6fbce
Denver Fernandes <sub367@gmail.com>
2024-03-10 00:18:22 +0000

    Adjust layouts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
+<svelte:head>
+    <title>Easy PGP</title> 
+</svelte:head>
+
 <div class="flex flex-col space-y-12">
     <div class="border-b border-gray-900/10 pb-12 m-8">
         <div class="mt-10 flex flex-col place-content-center">
-            <p>Welcome to PGP Tool.</p>
-            <p>This tool will help you encrypt and decrypt your messages.</p>
+            <h1 class="text-xl text-gray-500">Welcome to PGP Tool.</h1>
+            <p class="text-base text-gray-600">This tool will help you <a class="text-gray-900" href="/encrypt">encrypt</a> and <a class="text-gray-900" href="/decrypt">decrypt</a> your messages.</p>
         </div>
     </div>
 </div>
